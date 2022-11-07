f39739c64fed6e97b56ae15c736e1efe85e3eddf
Michał Szymczyk <szymczykm6936@o2.pl>
2022-11-07 13:19:03 +0100

    feat: add navbar component

--- src/ElegantStore.Web/src/app/app.module.ts
+++ src/ElegantStore.Web/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
