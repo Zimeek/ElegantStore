89f4694dfb5e47e920dfc2ae8aa42542d20867c2
Michał Szymczyk <szymczykm6936@o2.pl>
2022-11-10 13:15:34 +0100

    chore: new endpoint

--- src/ElegantStore.Web/src/app/core/services/product.service.ts
+++ src/ElegantStore.Web/src/app/core/services/product.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {delay, Observable} from "rxjs";
 import {Product} from "../models/product";
 
 @Injectable({
@@ -12,5 +12,9 @@ export class ProductService {
 
   public getFeaturedProducts(): Observable<Product[]> {
     return this.httpClient.get<Product[]>('api/Products/Featured');
+  }
+
+  public getProductById(id: number): Observable<Product> {
+    return this.httpClient.get<Product>(`api/Products/${id}`);
   }
 }
