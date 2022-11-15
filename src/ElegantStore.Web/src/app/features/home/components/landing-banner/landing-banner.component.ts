047426c2e6fed873b2ea6a89ee57733baad79015
Michał Szymczyk <szymczykm6936@o2.pl>
2022-11-15 22:49:03 +0100

    fix: add proper router navigation

--- src/ElegantStore.Web/src/app/features/home/components/landing-banner/landing-banner.component.ts
+++ src/ElegantStore.Web/src/app/features/home/components/landing-banner/landing-banner.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import {CommonModule} from "@angular/common";
+import {RouterModule} from "@angular/router";
 
 @Component({
   selector: 'app-landing-banner',
+  imports: [CommonModule, RouterModule],
   standalone: true,
   templateUrl: './landing-banner.component.html',
   styleUrls: ['./landing-banner.component.css']
