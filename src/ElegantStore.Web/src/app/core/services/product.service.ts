5119aeba215a25b6c073c86e54bd52ee0c36c448
Michał Szymczyk <szymczykm6936@o2.pl>
2022-11-15 22:49:19 +0100

    feat: add endpoint

--- src/ElegantStore.Web/src/app/core/services/product.service.ts
+++ src/ElegantStore.Web/src/app/core/services/product.service.ts
@@ -14,6 +14,10 @@ export class ProductService {
     return this.httpClient.get<Product[]>('api/Products/Featured');
   }
 
+  public getProductsPaged(page: number, pageSize: number, gender: string): Observable<Product[]> {
+    return this.httpClient.get<Product[]>(`api/Products?page=${page}&pageSize=${pageSize}&gender=${gender}`);
+  }
+
   public getProductById(id: number): Observable<Product> {
     return this.httpClient.get<Product>(`api/Products/${id}`);
   }
