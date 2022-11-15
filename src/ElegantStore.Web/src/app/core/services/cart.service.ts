9f4d0e5c9dcb07d25ca217c10b2fe618ab3bf481
Michał Szymczyk <szymczykm6936@o2.pl>
2022-11-15 22:51:19 +0100

    refactor: reactive approach

--- src/ElegantStore.Web/src/app/core/services/cart.service.ts
+++ src/ElegantStore.Web/src/app/core/services/cart.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, ReplaySubject} from "rxjs";
+import {BehaviorSubject, map, Observable} from "rxjs";
 import {Cart} from "../models/cart";
 import {AddCartItemRequest} from "../requests/add-cart-item-request";
 import {CartItem} from "../models/cart-item";
 import {UpdateCartItemRequest} from "../requests/update-cart-item-request";
-
+//DONE
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
-  private cartSubject = new ReplaySubject<Cart>(1);
-  private cart$ = this.cartSubject.asObservable();
+  private cartItems$ = new BehaviorSubject<CartItem[]>([]);
 
   constructor(private httpClient: HttpClient) { }
 
@@ -23,23 +22,58 @@ export class CartService {
     return localStorage.getItem('cartId');
   }
 
-  public loadCart(): Observable<Cart> {
-    return this.httpClient.get<Cart>('api/Cart');
+  public loadItems(): Observable<void> {
+    return this.httpClient.get<Cart>('api/Cart')
+      .pipe(map((cart: Cart) => {
+        if(this.getCartId() === null) {
+          this.setCartId(cart.id);
+        }
+        this.cartItems$.next(cart.items);
+      }));
   }
 
-  public getCart(): Observable<Cart> {
-    return this.cart$;
+  public getItems(): Observable<CartItem[]> {
+    return this.cartItems$.asObservable();
   }
 
-  public addItem(request: AddCartItemRequest): Observable<CartItem> {
-    return this.httpClient.post<CartItem>('api/Cart', request);
+  public getTotal(): Observable<number> {
+    return this.getItems()
+      .pipe(map(items => items.reduce((a, b) => {
+        return a + (b.quantity * b.price)
+      }, 0)))
   }
 
-  public updateItem(request: UpdateCartItemRequest): Observable<CartItem> {
-    return this.httpClient.put<CartItem>('api/Cart', request);
+  public addItem(request: AddCartItemRequest): Observable<void> {
+    const item = this.cartItems$.value.find(item => item.productId === request.productId && item.color === request.color);
+    if(item !== undefined) {
+      const updateRequest: UpdateCartItemRequest = { itemId: item.id, quantity: item.quantity + 1 };
+
+      return this.updateItem(updateRequest);
+    }
+
+    return this.httpClient.post<CartItem>('api/Cart', request)
+      .pipe(map(newItem => {
+        this.cartItems$.next([...this.cartItems$.value, newItem])
+      }))
+  }
+
+  public updateItem(request: UpdateCartItemRequest): Observable<void> {
+    return this.httpClient.put<CartItem>('api/Cart', request)
+      .pipe(map(updatedItem => {
+        const updatedItems = this.cartItems$.value.map(item =>
+          item.id === request.itemId ? { ...item, quantity: updatedItem.quantity } : item
+        );
+
+        this.cartItems$.next(updatedItems);
+      }))
   }
 
   public removeItem(itemId: string): Observable<Object> {
-    return this.httpClient.delete(`api/Cart/${itemId}`);
+    const updatedItems = this.cartItems$.value.filter(item => item.id !== itemId);
+
+    this.cartItems$.next(updatedItems);
+
+    return this.httpClient.delete(`api/Cart/${itemId}`)
+
   }
 }
