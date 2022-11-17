5a12ee010af1d81a0553db6498bb5abc761765eb
Michał Szymczyk <szymczykm6936@o2.pl>
2022-11-17 14:45:35 +0100

    perf: prevent multiple unnecessary api calls

--- src/ElegantStore.Web/src/app/core/services/product.service.ts
+++ src/ElegantStore.Web/src/app/core/services/product.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {delay, Observable} from "rxjs";
+import {BehaviorSubject, map, Observable, of} from "rxjs";
 import {Product} from "../models/product";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
+  private products$ = new BehaviorSubject<Product[]>([]);
 
   constructor(private httpClient: HttpClient) { }
 
@@ -19,6 +20,14 @@ export class ProductService {
   }
 
   public getProductById(id: number): Observable<Product> {
-    return this.httpClient.get<Product>(`api/Products/${id}`);
+    if(this.products$.value.some(product => product.id === id)) {
+      return of(this.products$.value.find(product => product.id === id) as Product);
+    }
+
+    return this.httpClient.get<Product>(`api/Products/${id}`)
+      .pipe(map(product => {
+        this.products$.next([...this.products$.value, product]);
+        return product;
+      }))
   }
 }
