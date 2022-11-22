296e148c0588e9e8f96b0c1a133553bf7f4a2b9c
Michał Szymczyk <szymczykm6936@o2.pl>
2022-11-22 13:27:20 +0100

    style: make mobile navbar menu toggleable

--- src/ElegantStore.Web/src/assets/js/custom.js
+++ src/ElegantStore.Web/src/assets/js/custom.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
   openOrCloseCart();
+  toggleMobileMenu();
 })
 
 function openOrCloseCart() {
@@ -8,11 +9,21 @@ function openOrCloseCart() {
   if(cart && cartOverlay) {
     let cartToggles = document.querySelectorAll('.cart-toggle');
     cartToggles.forEach(item => {
-      item.addEventListener('click', event => {
+      item.addEventListener('click', () => {
         cart.classList.toggle('translate-x-0');
         cart.classList.toggle('translate-x-full');
         cartOverlay.classList.toggle('hidden');
       });
     });
+  }
+}
+
+function toggleMobileMenu() {
+  const menu = document.querySelector('#mobile-menu');
+  const toggleButton = document.querySelector('#mobile-menu-toggle');
+  if(menu && toggleButton) {
+    toggleButton.addEventListener('click', () => {
+      menu.classList.toggle('hidden');
+    })
   }
 }
