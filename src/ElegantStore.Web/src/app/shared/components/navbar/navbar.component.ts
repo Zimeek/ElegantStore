2aa19093c486c5fb918833412b4aae0e3435d3e2
Michał Szymczyk <szymczykm6936@o2.pl>
2022-11-23 19:43:42 +0100

    style: add cart total next to cart icon

--- src/ElegantStore.Web/src/app/shared/components/navbar/navbar.component.ts
+++ src/ElegantStore.Web/src/app/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CartService} from "../../../core/services/cart.service";
+import {Observable} from "rxjs";
 
 @Component({
   selector: 'app-navbar',
@@ -6,10 +8,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  cartTotal$!: Observable<number>;
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartTotal$ = this.cartService.getTotal();
   }
 
 }
